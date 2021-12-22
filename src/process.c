94fbf2c107a9d5507fb402a8e24dd800d9dc20a3
dangcuong-123 <dangcuonga2songda@gmail.com>
2021-12-23 01:59:59 +0700

    login

--- src/process.c
+++ src/process.c
@@ -1,19 +1,6 @@
-typedef struct _User{
-    char usename[MAX];
-    char password[MAX];
-    struct _User *pNext;
-} User;
+#include "init.h"
 
-typedef struct List{
-    User *pHead;
-    User *pTail;
-} List;
-
-void InitList(List *l){
-    l->pHead = l->pTail = NULL;
-}
-
-int string_split (const char *str, char c, char ***arr)
+int string_split(const char *str, char c, char ***arr)
 {
     int count = 1;
     int token_len = 1;
@@ -29,7 +16,7 @@ int string_split (const char *str, char c, char ***arr)
         p++;
     }
 
-    *arr = (char**) malloc(sizeof(char*) * count);
+    *arr = (char **)malloc(sizeof(char *) * count);
     // if (*arr == NULL)
     //     exit(1);
 
@@ -38,7 +25,7 @@ int string_split (const char *str, char c, char ***arr)
     {
         if (*p == c)
         {
-            (*arr)[i] = (char*) malloc( sizeof(char) * token_len );
+            (*arr)[i] = (char *)malloc(sizeof(char) * token_len);
             // if ((*arr)[i] == NULL)
             //     exit(1);
 
@@ -48,7 +35,7 @@ int string_split (const char *str, char c, char ***arr)
         p++;
         token_len++;
     }
-    (*arr)[i] = (char*) malloc( sizeof(char) * token_len );
+    (*arr)[i] = (char *)malloc(sizeof(char) * token_len);
     // if ((*arr)[i] == NULL)
     //     exit(1);
 
@@ -74,80 +61,48 @@ int string_split (const char *str, char c, char ***arr)
     return count;
 }
 
-void readFile(char *file_name, char){
-    FILE *fin = fopen(file_name, "r");
-
-    if (fin == NULL) {
+void readUserFromFile()
+{
+    FILE *fin = fopen("./data/datauser.txt", "r");
+    if (fin == NULL)
+    {
         printf("Can't open file\n");
         return;
     }
-    char *token;
-    char tmp[MAX];
-    char tmp2[MAX];
-    char usename[MAX];
-    char password[MAX];
-    int status;
-    int win_times;
-    const char space[2] = " ";
-
-    while(!feof(fin)){
-        fgets(tmp2, 256, fin);
-        token = strtok(tmp2, space);
-        strcpy(usename, token);
-        token = strtok(NULL, space);
-        strcpy(password, token);
-        token = strtok(NULL, space);
-        strcpy(tmp, token);
-        if(strlen(tmp) > 1){
-            status = (tmp[0] - '0')*10 + (tmp[1] - '0');
-        }
-        else status = tmp[0] - '0';
-        token = strtok(NULL, space);
-        strcpy(tmp, token);
-        // printf("%s---\n",tmp);
-        if(tmp[strlen(tmp)-1] == '\n')  tmp[strlen(tmp)-1] = '\0';
-        if(strlen(tmp) > 1){
-            win_times = (tmp[0] - '0')*10 + (tmp[1] - '0');
-        }
-        else win_times = tmp[0] - '0';
-        User *p = makeUser(usename, password, status, win_times);
-        addUser(l, p);
+    char readFile[MAX_LENGTH];
+    char **arr;
+    int id = -1;
+    while (fgets(readFile, MAX_LENGTH, fin))
+    {
+        id++;
+        int count = string_split(readFile, ' ', &arr);
+        strcpy(listUsers[id].username, arr[0]);
+        strcpy(listUsers[id].password, arr[1]);
     }
+    numberUsers = id + 1;
+
     fclose(fin);
 }
 
-void sortListStatus(List *clas){
-    List stu1, stu2;
-    if(clas->pHead == clas->pTail) return;
-    InitList(&stu1);  InitList(&stu2);
-    User *p;
-    User *tag;
-    tag = clas->pHead;
-    clas->pHead = clas->pHead->pNext;
-    tag->pNext = NULL;
-    while(clas->pHead != NULL){
-        p = clas->pHead;
-        clas->pHead = clas->pHead->pNext;
-        p->pNext = NULL;
-        if(p->status >= tag->status) {
-            addUser(&stu1, p);
+int checkSignIn(char *username, char *password)
+{
+    for (int i = 0; i < numberUsers; i++)
+    {
+        if(strcmp(username, listUsers[i].username) == 0)
+        {
+            if(strcmp(password, listUsers[i].password) == 0) 
+                // correct user
+                return 1;
+            else 
+                // wrong password
+                return 0;
         }
-        else addUser(&stu2, p);
     }
-    sortListStatus(&stu1);
-    sortListStatus(&stu2);
-    if(stu1.pHead != NULL){
-        clas->pHead = stu1.pHead;
-        stu1.pTail->pNext = tag;
-    }
-    else{
-        clas->pHead = tag;
-    }
-    tag->pNext = stu2.pHead;
-    if(stu2.pHead != NULL){
-        clas->pTail = stu2.pTail;
-    }
-    else{
-        clas->pTail = tag;
-    }
+    // not find users
+    return -1;
+}
+
+void sortScore()
+{
+    return;
 }
--- src/server.c
+++ src/server.c
@@ -1,61 +1,74 @@
 #include "init.h"
 
-void *connection_handler(void *client_socket){
-	int socket = *(int*) client_socket;
-	int read_len;
-	char client_message[100];
+void *connection_handler(void *client_socket)
+{
+	int socket = *(int *)client_socket;
 	int send_status;
+	int read_len;
 	char **arr = NULL;
 	int count;
-	while( (read_len=recv(socket,client_message, 100,0))>0)
+	while ((read_len = recv(socket, messageServer, MESSAGE_MAX, 0)) > 0)
 	{
-		//end of string marker
-		client_message[read_len] = '\0';
-		printf("String receive from client: %s\n", client_message);
+		printf("String receive from client: %s\n", messageServer);
 
-		count = string_split(client_message, ' ', &arr);
-		if (strcmp(arr[0], "1") == 0) {
-			
+		count = string_split(messageServer, ' ', &arr);
+		if (strcmp(arr[0], "1") == 0)
+		{
+			int check = checkSignIn(arr[1], arr[2]);
+			if (check == 1)
+			{
+				strcpy(messageServer, "1");
+				ServerSendToClient(socket);
+			}
+			else
+			{
+				strcpy(messageServer, "0");
+				ServerSendToClient(socket);
+			}
 		}
-		
-		//Send the message back to client
-		send_status=send(socket , client_message , strlen(client_message),0);
+
+		// Send the message back to client
+		//  send_status=send(socket , message , strlen(message),0);
 	}
-	
+
 	return 0;
 }
 
 int main(int argc, char *argv[])
 {
-    
-    int server_socket = ServerCreateSocket(atoi(argv[1]));
-    int no_threads=0;
-    pthread_t threads[MAX_PLAYER];
-    while (no_threads < MAX_PLAYER){
+	readUserFromFile();
+	int server_socket = ServerCreateSocket(atoi(argv[1]));
+	int no_threads = 0;
+	pthread_t threads[MAX_PLAYER];
+	while (no_threads < MAX_PLAYER)
+	{
 		printf("Listening...\n");
 		int client_socket = accept(server_socket, NULL, NULL);
 		puts("Connection accepted");
-		if( pthread_create( &threads[no_threads], NULL ,  connection_handler , &client_socket) < 0){
+		if (pthread_create(&threads[no_threads], NULL, connection_handler, &client_socket) < 0)
+		{
 			perror("Could not create thread");
 			return 1;
 		}
-		if (client_socket < 0) { 
-			printf("server acccept failed...\n"); 
-			exit(0); 
-			} 
+		if (client_socket < 0)
+		{
+			printf("server acccept failed...\n");
+			exit(0);
+		}
 		else
 			printf("Server acccept the client...\n");
 		puts("Handler assigned");
 		no_threads++;
 	}
-	int k=0;
-    for (k=0;k<MAX_PLAYER;k++){
-		pthread_join(threads[k],NULL);
+	int k = 0;
+	for (k = 0; k < MAX_PLAYER; k++)
+	{
+		pthread_join(threads[k], NULL);
 	}
 
-    //int send_status;
-    //send_status=send(client_socket, server_message, sizeof(server_message), 0);
-    close(server_socket);
-    
-    return 0;
+	// int send_status;
+	// send_status=send(client_socket, server_message, sizeof(server_message), 0);
+	close(server_socket);
+
+	return 0;
 }
